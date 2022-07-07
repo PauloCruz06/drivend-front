f89ec143fc8f99ab25295fa9c10e0d82aa56f959
Paulo <pc5783346@gmail.com>
2022-07-06 21:37:51 -0300

    added style.css

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import reactDom from "react-dom";
 import App from './components/App';
 import "./assets/reset.css";
+import "./assets/style.css";
 
 reactDom.render(<App/>, document.querySelector(".root"));
