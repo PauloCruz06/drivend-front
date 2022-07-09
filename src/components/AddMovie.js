6c0dceb4e593c61531bf147abd8c43584e7ec4ea
Paulo <pc5783346@gmail.com>
2022-07-09 15:35:02 -0300

    feat: added function that adds movies for sale

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,118 +1,90 @@
 import styled from 'styled-components';
 import axios from 'axios';
-import { Link, useNavigate } from "react-router-dom";
-import {React, useContext, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { React, useContext, useState } from 'react';
 import UserContext from "../context/UserContext";
-import { ThreeDots } from  'react-loader-spinner';
-import dayjs from 'dayjs';
+import { ThreeDots } from 'react-loader-spinner';
 import dotenv from 'dotenv';
 
-export default function LoginScreen(){
+export default function LoginScreen() {
 
     dotenv.config();
 
-    //const [productId, setProductId]=useState("");
-    const [title, setTitle]=useState("");
-    const [value, setValue]=useState("");
-    const [director, setDirector]=useState("");
-    const [year, setYear]=useState("");
-    const [genre, setGenre]=useState([]);
-    const [pr, setPr]=useState("");
-    const [quantity, setQuantity]=useState("");
-    const [selleremail, setSelleremail]=useState("");
-    const [image, setImage]=useState("");
-    const [description,setDescription]=useState("");
+    const [title, setTitle] = useState("");
+    const [value, setValue] = useState("");
+    const [director, setDirector] = useState("");
+    const [year, setYear] = useState("");
+    const [genre, setGenre] = useState("");
+    const [pr, setPr] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [image, setImage] = useState("");
+    const [description, setDescription] = useState("");
 
     const [isLoading, setIsLoading] = useState(false);
 
     const navigate = useNavigate();
 
-    const { user, setUser } = useContext(UserContext);
-    const {token} = user;
+    const { user } = useContext(UserContext);
+    const { token } = user;
 
     const config = {
         headers: {
-          Authorization: `Bearer ${token}`
+            Authorization: `Bearer ${token}`
         }
     };
 
-    function Send(event){
+    function Send(event) {
         event.preventDefault();
         setIsLoading(true);
-        const postMovie={
-            //productId,
+        const body = {
             title,
             value,
             director,
             year,
-            genre,
+            genre: genre.split(" "),
             pr,
             quantity,
-            selleremail,
+            selleremail: user.email,
             image,
             description
-            
         }
 
-        //const promise=axios.post(`http://localhost:5000/transaction`, postMovie, config);
-        const promise=axios.post(`${process.env.REACT_APP_URL_API}/movies`, postMovie, config);
+        const promise = axios.post(`${process.env.REACT_APP_URL_API}/movies`, body, config);
 
-        promise.then(resposta => {
-           // setProductId("");
-            setTitle("");
-            setValue("");
-            setDirector("");
-            setYear("");
-            setGenre("");
-            setPr("");
-            setQuantity("");
-            setSelleremail("");
-            setImage("");
-            setDescription("");
+        promise.then(() => {
             setIsLoading(false);
             navigate("/");
+        }).catch((e) => {
+            console.log(e);
+            alert("não foi possível cadastrar filme");
+            setIsLoading(false);
         });
     }
-    
-    return(
+
+    return (
         <>
-        <Header>
-            <h1>Adicionar novo filme:</h1>  
-            <ion-icon onClick={()=>navigate("/perfilusuario")} name="return-down-back-outline"></ion-icon>
-        </Header>
-        <Container>
-            {isLoading ? (
-                <Form background={"#f2f2f2"} color={"#afafaf"}>
-                    {/* <input  disabled id="productId" value={productId} placeholder="id do produto" required onChange={(e)=>setProductId(e.target.value)} /> */}
-                    <input  disabled id="title" value={title} placeholder="titulo do filme" required onChange={(e)=>setTitle(e.target.value)} />
-                    <input  disabled id="value" type="number" value={value} placeholder="valor" required onChange={(e)=>setValue(e.target.value)} />
-                    <input  disabled id="director" value={director} placeholder="diretor do filme" required onChange={(e)=>setDirector(e.target.value)} />
-                    <input  disabled id="year" type="number" value={year} placeholder="ano de lançamento" required onChange={(e)=>setYear(e.target.value)} />
-                    <input  disabled id="genre" value={genre} placeholder="genero do filme" required onChange={(e)=>setGenre(e.target.value)} />
-                    <input  disabled id="pr" type="number" value={pr} placeholder="idade minima" required onChange={(e)=>setPr(e.target.value)} />
-                    <input  disabled id="quantity" type="number" value={quantity} placeholder="quantidade" required onChange={(e)=>setQuantity(e.target.value)} />
-                    <input  disabled id="selleremail" value={selleremail} placeholder="email do vendedor" required onChange={(e)=>setSelleremail(e.target.value)} />
-                    <input  disabled id="image" value={image} placeholder="imagem do filme" required onChange={(e)=>setImage(e.target.value)} />
-                    <input  disabled id="description" value={description} placeholder="descrição" required onChange={(e)=>setDescription(e.target.value)} />
-                    <button type="submit" disabled opacity={0.7}>{<ThreeDots color={"#ffffff"} width={51} />}</button>
-                </Form>
-                 ) : ( 
+            <Header>
+                <h1>Adicionar novo filme:</h1>
+                <ion-icon onClick={() => navigate("/perfilusuario")} name="return-down-back-outline"></ion-icon>
+            </Header>
+            <Container>
                 <Form background={"#ffffff"} color={"#000000"} onSubmit={Send}>
-                    {/* <input  id="productId" value={productId} placeholder="id do produto" required onChange={(e)=>setProductId(e.target.value)} /> */}
-                    <input  id="title" value={title} placeholder="titulo do filme" required onChange={(e)=>setTitle(e.target.value)} />
-                    <input  id="value" type="text" value={value} placeholder="valor" required onChange={(e)=>setValue(e.target.value)} />
-                    <input  id="director" value={director} placeholder="diretor do filme" required onChange={(e)=>setDirector(e.target.value)} />
-                    <input  id="year" type="text" value={year} placeholder="ano de lançamento" required onChange={(e)=>setYear(e.target.value)} />
-                    <input  id="genre" value={genre} placeholder="genero do filme" required onChange={(e)=>setGenre(e.target.value)} />
-                    <input  id="pr" type="number" value={pr} placeholder="idade minima" required onChange={(e)=>setPr(e.target.value)} />
-                    <input  id="quantity" type="number" value={quantity} placeholder="quantidade" required onChange={(e)=>setQuantity(e.target.value)} />
-                    <input  id="selleremail" value={selleremail} placeholder="email do vendedor" required onChange={(e)=>setSelleremail(e.target.value)} />
-                    <input  id="image" value={image} placeholder="imagem do filme" required onChange={(e)=>setImage(e.target.value)} />
-                    <input  id="description" value={description} placeholder="descrição" required onChange={(e)=>setDescription(e.target.value)} />
-                    <button type="submit">Enviar entrada</button>
+                    <input id="title" type="text" value={title} placeholder="titulo do filme" required onChange={(e) => setTitle(e.target.value)} />
+                    <input id="value" type="text" pattern="^[0-9]*\.[0-9]{2}$" value={value} placeholder="valor" required onChange={(e) => setValue(e.target.value)} />
+                    <input id="director" type="text" value={director} placeholder="diretor do filme" required onChange={(e) => setDirector(e.target.value)} />
+                    <input id="year" type="text" value={year} pattern="^[0-9]{4}$" placeholder="ano de lançamento" required onChange={(e) => setYear(e.target.value)} />
+                    <input id="genre" value={genre} placeholder="generos do filme" required onChange={(e) => setGenre(e.target.value)} />
+                    <input id="pr" type="number" value={pr} placeholder="idade minima" required onChange={(e) => setPr(e.target.value)} />
+                    <input id="quantity" type="number" value={quantity} placeholder="quantidade" required onChange={(e) => setQuantity(e.target.value)} />
+                    <input id="image" type="url" value={image} placeholder="imagem do filme" required onChange={(e) => setImage(e.target.value)} />
+                    <input id="description" type="text" value={description} placeholder="descrição" required onChange={(e) => setDescription(e.target.value)} />
+                    {isLoading ?
+                        <div type="submit" disabled opacity={0.7}>{<ThreeDots color={"#ffffff"} width={51} />}</div>
+                        :
+                        <button type="submit">Enviar entrada</button>
+                    }
                 </Form>
-            )}
-        </Container>
+            </Container>
         </>
     )
 }
@@ -144,7 +116,7 @@ const Form = styled.form`
         font-size: 20px;
         font-style: italic;
     }
-    button {
+    button, div {
         font-family: 'Raleway';
         font-weight: 700;
         min-width: 100px;
@@ -152,7 +124,7 @@ const Form = styled.form`
         margin-right: 36px;
         margin-left: 36px;
         text-align: center;
-        background-color: #A328D6;
+        background-color: #E10E0E;
         color: #FFFFFF;
         font-size: 21px;
         border: none;
@@ -165,12 +137,12 @@ const Form = styled.form`
         }
     }
 `
-const Header=styled.div`
+const Header = styled.div`
     display: flex;
     align-items: center;
     width: 100%;
     height: 70px;
-    background-color:#8C11BE ;
+    background-color:#E10E0E ;
     justify-content: space-between;
     position:fixed;
     top:0;
@@ -192,7 +164,7 @@ const Header=styled.div`
         border-radius: 98.5px;
     }
 `
-const Container=styled.div`
+const Container = styled.div`
 display: flex;
 align-items: center;
 justify-content: center;
